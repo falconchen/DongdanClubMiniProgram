111a55befca0fccf017d8056beb416a177eb6d9c
falcon <falcon_chen@qq.com>
2019-04-21 15:40:52 +0800

    fix poster

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -4,7 +4,7 @@ var modalImg = require('../template/modal-img.js');
 const commentPerPage = 20;
 var currentPage = 1;
 var app = getApp();
-var finishLoadComments =false;
+var finishLoadComments = false;
 
 Page({
 
@@ -17,17 +17,17 @@ Page({
     tweet: {},
     replies: [],
     replyPage: 0,
-    tweetId: '',    
+    tweetId: '',
     commentCount: 0,
     tweetBodyHtml: '',
-    comments: [],    
+    comments: [],
     finishLoadComments: false,
     commentPerPage: currentPage,
-    tweetData :'',
-    bookmarked : false,
-    finishLoadList:true, //第一页评论不加载loading icon
-    skinStyle:'',
-    posterUrl:''
+    tweetData: '',
+    bookmarked: false,
+    finishLoadList: true, //第一页评论不加载loading icon
+    skinStyle: '',
+    posterUrl: ''
   },
   clickLink: util.clickLink,
 
@@ -37,8 +37,14 @@ Page({
     wx.setNavigationBarTitle({
       title: tweet.author + ' 的动弹'
     });
-    
-    this.setData({ tweet: tweet, tweetBodyHtml: tweetBodyHtml, commentCount: tweet.commentCount, tweetData: JSON.stringify(tweet),bookmarked:this.isBookmarked(tweet.id)});
+
+    this.setData({
+      tweet: tweet,
+      tweetBodyHtml: tweetBodyHtml,
+      commentCount: tweet.commentCount,
+      tweetData: JSON.stringify(tweet),
+      bookmarked: this.isBookmarked(tweet.id)
+    });
 
   },
   /**
@@ -48,62 +54,65 @@ Page({
     var that = this;
     util.changeBarTabStyle();
     that.setData({
-      skinStyle:app.globalData.skin
+      skinStyle: app.globalData.skin
     });
 
-    that.tweetId = options.id;    
+    that.tweetId = options.id;
     that.key = 'tweet_' + that.tweetId;
 
 
     wx.getStorage({
-      
-      
+
+
       key: that.key,
       success: function (res) {
-                              
+
         //var tweetBodyHtml = app.towxml.toJson(res.data.body, 'html');  
-        
-        that.initTweet(res.data);   
+
+        that.initTweet(res.data);
         wx.removeStorage({
           key: that.key,
-          success: function (res) {            
-          }
+          success: function (res) {}
         });
 
       },
-      fail:function(){
-        
+      fail: function () {
 
-        util.getTweetDetail(that.tweetId, function (data) { 
-                                    
+
+        util.getTweetDetail(that.tweetId, function (data) {
+
           that.initTweet(data)
 
-        }, function () { }, function () { });
+        }, function () {}, function () {});
       },
-      complete:function(){
-              
-        
+      complete: function () {
+
+
         //显示评论
         currentPage = 1;
         util.getTweetCommentList(that.tweetId, currentPage, commentPerPage,
           function (resdata) {
             finishLoadComments = Boolean(resdata.length < commentPerPage)
-                                  
+
             //that.rawComments = resdata;
             //that.rawComments = JSON.parse(JSON.stringify(resdata)); //深层复制
             resdata = util.blockCommentFilter(resdata);
-                        
-            that.setData({ comments: resdata, finishLoadComments: finishLoadComments ,finishLoadList: finishLoadComments})
+
+            that.setData({
+              comments: resdata,
+              finishLoadComments: finishLoadComments,
+              finishLoadList: finishLoadComments
+            })
             currentPage++
           },
-          function () { }
+          function () {}
         );
       }
 
     })
     //console.log(options);
   },
-  
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -119,7 +128,7 @@ Page({
     var that = this;
     util.changeBarTabStyle();
     that.setData({
-      skinStyle:app.globalData.skin
+      skinStyle: app.globalData.skin
     });
   },
 
@@ -148,10 +157,10 @@ Page({
 
     // })
     // return;//暂时禁止
-    
+
     // var that = this;
-    
-    
+
+
     // wx.reLaunch({
     //   url: 'detail?id='+that.tweetId,
     // });
@@ -162,13 +171,15 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
     var that = this;
     //console.log(that.data.finishLoadComments);return;
-    
+
     if (finishLoadComments) {
-      
-      that.setData({  finishLoadComments: true })
+
+      that.setData({
+        finishLoadComments: true
+      })
       return;
     }
 
@@ -177,15 +188,18 @@ Page({
     util.getTweetCommentList(that.tweetId, currentPage, commentPerPage,
       function (resdata) {
         finishLoadComments = Boolean(resdata.length < commentPerPage)
-        
+
 
         //wx.hideLoading()
 
         var commentData = that.data.comments.concat(resdata);
         commentData = util.blockCommentFilter(commentData);
-        that.setData({ comments: commentData, finishLoadComments: finishLoadComments })
+        that.setData({
+          comments: commentData,
+          finishLoadComments: finishLoadComments
+        })
         currentPage++
-        
+
 
       },
       function () {
@@ -205,27 +219,29 @@ Page({
   /**
    * 点击收藏
    */
-  bookmark: function(e) {
+  bookmark: function (e) {
     var that = this;
 
     var tweet = JSON.parse(e.currentTarget.dataset.tweet);
     var bookmarklist = wx.getStorageSync('bookmarklist') || [];
     bookmarklist.unshift(tweet);
     wx.setStorageSync('bookmarklist', bookmarklist);
-    this.setData({ bookmarked: this.isBookmarked(tweet.id) });
+    this.setData({
+      bookmarked: this.isBookmarked(tweet.id)
+    });
 
   },
 
   /**
    * 检查是否被收藏
    */
-  isBookmarked: function(tweetid) {
+  isBookmarked: function (tweetid) {
     var bookmarklist = wx.getStorageSync('bookmarklist') || [];
     if (bookmarklist.length == 0) {
       return false;
     }
-    for(var i=0;i<bookmarklist.length;i++) {
-      if(bookmarklist[i].id == tweetid) {
+    for (var i = 0; i < bookmarklist.length; i++) {
+      if (bookmarklist[i].id == tweetid) {
         return true;
       }
     }
@@ -235,33 +251,35 @@ Page({
   /**
    * 取消收藏
    */
-  unbookmark: function(e) {
+  unbookmark: function (e) {
     var that = this;
-    
+
     var tweet = JSON.parse(e.currentTarget.dataset.tweet);
     var bookmarklist = wx.getStorageSync('bookmarklist') || [];
-    if( bookmarklist.length == 0 ) {
+    if (bookmarklist.length == 0) {
       return;
-    }    
-    for(var i=0;i<bookmarklist.length;i++) {
+    }
+    for (var i = 0; i < bookmarklist.length; i++) {
 
       if (tweet.id == bookmarklist[i].id) {
-        
+
         bookmarklist.splice(i, 1);
         wx.setStorageSync('bookmarklist', bookmarklist);
-        
-        that.setData({ bookmarked: that.isBookmarked(tweet.id) });
+
+        that.setData({
+          bookmarked: that.isBookmarked(tweet.id)
+        });
         return;
       }
     }
-    return ;
-    
+    return;
+
 
   },
   /**
    * 屏蔽评论中的头像
    */
-  blockUserComment: function(e){
+  blockUserComment: function (e) {
     var that = this;
 
     var author = e.currentTarget.dataset.author;
@@ -289,7 +307,10 @@ Page({
           wx.setStorageSync('blocklist', blocklist);
           var commentData = that.data.comments;
           commentData = util.blockCommentFilter(commentData);
-          that.setData({ comments: commentData, finishLoadComments: finishLoadComments })
+          that.setData({
+            comments: commentData,
+            finishLoadComments: finishLoadComments
+          })
           return true;
         }
 
@@ -302,123 +323,140 @@ Page({
   /**
    * 生成海报
    */
-  makePoster: function(e){
+  makePoster: function (e) {
     var that = this;
     var tweet = JSON.parse(e.currentTarget.dataset.tweet);
     var path = "pages/detail/detail?id=" + tweet.id;
-    var qrcodeImagePath ='';
+    var qrcodeImagePath = '';
     var imageInlocalFlag = true;
     var posterImagePath = '';
     var title = tweet.author + '的动弹';
     var excerpt = util.stripTags(tweet.body);
-    var flag = false;
-
     
-    if( tweet.thumbs != undefined ){
-     
+    var flag = false;
+    var posterImageUrl = tweet.portrait;
+
+    if(excerpt.length > 90) {
+      excerpt = excerpt.substr(0,90) + '...';
+    }
+
+    if (tweet.bigImgs != undefined) {
+      posterImageUrl = tweet.bigImgs[0];
+
+    }
+    console.log(posterImageUrl);
+    if( posterImageUrl.indexOf('oscimg.oschina.net')>0 ){
+
+      var tailPos = posterImageUrl.indexOf('!/');
+      if(tailPos > 0) {
+        posterImageUrl.substr(0,tailPos) ;
+      }
+      //posterImageUrl += '!/both/400x270';
+      posterImageUrl += '!/sq/400';
+    }
+
+
     wx.showLoading({
       title: "正在生成海报",
       mask: true,
     });
 
-      imageInlocalFlag = false;
-      var posterImageUrl = tweet.thumbs[0];
-
-      //请求qrcode
-      util.getWechatQRcode(path,function(data){
-          if(data.success) {
-            var posterQrcodeUrl = data.data.url;
-
-            const downloadTaskQrcodeImage = wx.downloadFile({
-              url: posterQrcodeUrl,
-              success: res => {
-                  if (res.statusCode === 200) {
-                      qrcodeImagePath = res.tempFilePath;
-                      console.log("二维码图片本地位置：" + res.tempFilePath);
-                      
-                      if (!imageInlocalFlag) {
-                          const downloadTaskForPostImage = wx.downloadFile({
-                              url: posterImageUrl,
-                              success: res => {
-                                  if (res.statusCode === 200) {
-                                      posterImagePath = res.tempFilePath;
-                                      console.log("文章图片本地位置：" + res.tempFilePath);
-                                      flag = true;
-                                      if (posterImagePath && qrcodeImagePath) {
-                                          util.createPosterLocal(posterImagePath, qrcodeImagePath, title, excerpt,function(imgPath){
-                                              wx.hideLoading();
-                                                console.log(imgPath);
-                                                that.setData({posterUrl:imgPath});
-
-                                          },function(res){
-                                            wx.hideLoading();
-                                            console.log(res);
-                                          });
-                                      }
-                                  }
-                                  else {
-                                      console.log(res);
-                                      wx.hideLoading();
-                                      wx.showToast({
-                                          title: "生成海报失败...",
-                                          mask: true,
-                                          duration: 2000
-                                      });
-                                      return false;
+    imageInlocalFlag = false;
 
 
-                                  }
-                              }
+    //请求qrcode
+    util.getWechatQRcode(path, function (data) {
+      if (data.success) {
+        var posterQrcodeUrl = data.data.url;
+
+        const downloadTaskQrcodeImage = wx.downloadFile({
+          url: posterQrcodeUrl,
+          success: res => {
+            if (res.statusCode === 200) {
+              qrcodeImagePath = res.tempFilePath;
+              console.log("二维码图片本地位置：" + res.tempFilePath);
+
+              if (!imageInlocalFlag) {
+                const downloadTaskForPostImage = wx.downloadFile({
+                  url: posterImageUrl,
+                  success: res => {
+                    if (res.statusCode === 200) {
+                      posterImagePath = res.tempFilePath;
+                      console.log("文章图片本地位置：" + res.tempFilePath);
+                      flag = true;
+                      if (posterImagePath && qrcodeImagePath) {
+                        util.createPosterLocal(posterImagePath, qrcodeImagePath, title, excerpt, function (imgPath) {
+                          wx.hideLoading();
+                          console.log(imgPath);
+                          that.setData({
+                            posterUrl: imgPath
                           });
-                          downloadTaskForPostImage.onProgressUpdate((res) => {
-                              console.log('下载文章图片进度：' + res.progress)
 
-                          })
+                        }, function (res) {
+                          wx.hideLoading();
+                          console.log(res);
+                        });
                       }
-                      else {
-                          if (posterImagePath && qrcodeImagePath) {
-                            util.createPosterLocal(posterImagePath, qrcodeImagePath, title, excerpt,function(imgPath){
-                              wx.hideLoading();
-                              console.log(imgPath);
-                              that.setData({posterUrl:imgPath});
-                            },function(res){
-                              wx.hideLoading();
-                              console.log(res);
-                            });
-                          }
-                      }
-                  }
-                  else {
+                    } else {
                       console.log(res);
                       wx.hideLoading();
-                      flag = false;
                       wx.showToast({
-                          title: "生成海报失败...",
-                          mask: true,
-                          duration: 2000
+                        title: "生成海报失败...",
+                        mask: true,
+                        duration: 2000
                       });
                       return false;
+
+
+                    }
                   }
+                });
+                downloadTaskForPostImage.onProgressUpdate((res) => {
+                  console.log('下载图片进度：' + res.progress)
+
+                })
+              } else {
+                if (posterImagePath && qrcodeImagePath) {
+                  util.createPosterLocal(posterImagePath, qrcodeImagePath, title, excerpt, function (imgPath) {
+                    wx.hideLoading();
+                    console.log(imgPath);
+                    that.setData({
+                      posterUrl: imgPath
+                    });
+                  }, function (res) {
+                    wx.hideLoading();
+                    console.log(res);
+                  });
+                }
               }
-          });
-          downloadTaskQrcodeImage.onProgressUpdate((res) => {
-              console.log('下载二维码进度', res.progress)
-          })
-
-
-
+            } else {
+              console.log(res);
+              wx.hideLoading();
+              flag = false;
+              wx.showToast({
+                title: "生成海报失败...",
+                mask: true,
+                duration: 2000
+              });
+              return false;
+            }
           }
+        });
+        downloadTaskQrcodeImage.onProgressUpdate((res) => {
+          console.log('下载二维码进度', res.progress)
+        })
 
-      });
-      
-      
 
-    }
 
-    //console.log(tweet);
+      }else{
+
+      }
+
+    });
+
   },
 
-  
+
 
   blockCommentFilter: util.blockCommentFilter,
 
